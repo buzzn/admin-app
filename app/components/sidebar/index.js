abb607c57187c11938516041c494642c76e051e4
Felix Faerber <ffaerber@gmail.com>
2017-01-02 20:49:42 +0100

    static component

--- app/components/sidebar/index.js
+++ app/components/sidebar/index.js
@@ -4,21 +4,12 @@ import { Link } from 'react-router'
 
 import './style.scss';
 
-class Sidebar extends Component {
-  render() {
-    return (
-      <div className="col-sm-3 col-md-2 sidebar">
-        <ul className="nav nav-sidebar">
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/groups'>Groups</Link></li>
-          <li><Link to='/profiles'>Profiles</Link></li>
-          <li><Link to='/organizations'>Organizations</Link></li>
-          <li><Link to='/meters'>Meters</Link></li>
-          <li><Link to='/contracts'>Contracts</Link></li>
-        </ul>
-      </div>
-    );
-  }
-}
-
-export default Sidebar;
+export default () => (
+  <div className="col-sm-3 col-md-2 sidebar">
+    <ul className="nav nav-sidebar">
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/groups'>Groups</Link></li>
+      <li><Link to='/profiles'>Profiles</Link></li>
+    </ul>
+  </div>
+)
