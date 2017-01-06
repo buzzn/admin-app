b9f35492f0d01a7415130a9cae2d713e0c1861bd
Felix Faerber <ffaerber@gmail.com>
2017-01-06 12:26:15 +0100

    readme

--- app/profiles/components/profile.js
+++ app/profiles/components/profile.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { actions } from '../actions';
 
-export const Profile = ({ profile }) => {
-  if (!profile || profile.failed) return (<div>Profile not found</div>);
+import Meters from '../../meters'
 
-  if (profile.loading) return (<div>Loading...</div>);
+export class Profile extends Component {
+  componentWillMount() {
+    const { loadProfile, params: { id } } = this.props;
+    loadProfile(id);
+  }
 
-  return (
-    <div className="profile">
-      <h4>{ profile.firstName } { profile.lastName }</h4>
-      <p>{ profile.aboutMe }</p>
-    </div>
-  );
-};
+  render() {
+    const { profile, loading } = this.props;
 
-function mapStateToProps(state, props) {
+    if (loading) return (<div>Loading...</div>);
+
+    if (!profile) return (<div>Profile not found</div>);
+
+    return (
+      <div className="profile">
+        <h4>{ profile.attributes['first-name'] }</h4>
+        <p>{ profile.attributes['about-me'] }</p>
+
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
   return {
-    profile: state.profiles.profiles[props.userId],
+    profile: state.profiles.profile,
+    loading: state.profiles.loadingProfile,
   };
 }
 
-export default connect(mapStateToProps)(Profile);
+export default connect(mapStateToProps, { loadProfile: actions.loadProfile })(Profile);
