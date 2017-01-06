06c2276c07af02123662fe9fc40eed9651a7b024
ChipJuggler <scsi.rdx@gmail.com>
2017-01-06 17:43:00 +0700

    login page, navbar button, redirects

--- app/root.js
+++ app/root.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Match, Miss } from 'react-router';
+import { BrowserRouter, Match, Miss, Redirect } from 'react-router';
 import { connect } from 'react-redux';
 
 import MainNavBar from './components/main_nav_bar';
 import Sidebar from './components/sidebar';
+import SignIn from './components/sign_in';
 
 import Profile from './pages/profile';
 import Groups from './groups';
@@ -14,20 +15,23 @@ const Root = ({ token }) => (
   <BrowserRouter>
     <div>
       <MainNavBar signedIn={ !!token } />
-      <div className="container-fluid">
-        <Sidebar />
-        <div className='col-sm-9 offset-sm-3 col-md-10 offset-md-2 main'>
-          { token ?
+      { token ?
+        <div className="container-fluid">
+          <Sidebar />
+          <div className='col-sm-9 offset-sm-3 col-md-10 offset-md-2 main'>
             <div>
               <Match exactly pattern="/" component={ Profile } />
               <Match pattern="/profile/:id" component={ Profile } />
               <Match pattern="/groups" component={ Groups.Container } />
-            </div> :
-            <Match exactly pattern="/" render={ () => (<div>Please, log in to use this app.</div>) } />
-          }
-          <Miss render={ () => (<div>404</div>) } />
+            </div>
+          </div>
+        </div> :
+        <div className="container-fluid">
+          <Match exactly pattern="/" component={ SignIn } />
+          <Match pattern="*" render={ () => (<Redirect to="/" />) } />
         </div>
-      </div>
+      }
+      <Miss render={ () => (<div>404</div>) } />
     </div>
   </BrowserRouter>
 );
