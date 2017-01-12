f4230d3ff6e7c6354b79fe1dd8f4da84b7606848
ChipJuggler <scsi.rdx@gmail.com>
2017-01-12 16:19:13 +0700

    new ver of auth module

--- app/config.js
+++ app/config.js
@@ -5,18 +5,24 @@ if (process.env.NODE_ENV === 'production') {
     apiUrl: 'https://app.buzzn.net/',
     apiPath: 'api/v1',
     clientId: '',
+    scope: 'full',
+    callBackURL: '',
   };
 } else if (process.env.NODE_ENV === 'staging') {
   config = {
     apiUrl: 'https://staging.buzzn.net/',
     apiPath: 'api/v1',
     clientId: '',
+    scope: 'full',
+    callBackURL: '',
   };
 } else {
   config = {
     apiUrl: 'http://localhost:3000/',
     apiPath: 'api/v1',
-    clientId: '',
+    clientId: 'a2fe779b73cb3070fcf9ddbbefad2f37145fd959e09d5701efdcab07fb769178',
+    scope: 'full',
+    callBackURL: 'http://localhost:2999/',
   };
 }
 
