5b4bec87f43ee9766785fbda99a4b6a585c27156
ChipJuggler <scsi.rdx@gmail.com>
2017-01-13 00:39:01 +0700

    virtual meters fix

--- app/registers/api.js
+++ app/registers/api.js
@@ -4,10 +4,10 @@ import { prepareHeaders, parseResponse } from '../_util';
 export default {
   fetchMeterRegisters({ token, apiUrl, apiPath, meterId, meterType }) {
     // FIXME: temp slice workaround, see https://github.com/buzzn/buzzn/issues/680
-    return fetch(`${apiUrl}${apiPath}/meters/${meterType.slice(0, -1)}/${meterId}/registers`, {
+    return fetch(`${apiUrl}${apiPath}/meters/${meterType.slice(0, -1)}/${meterId}/register${meterType === 'reals' ? 's' : ''}`, {
       headers: prepareHeaders(token),
     })
     .then(parseResponse)
-    .then(json => json.data);
+    .then(json => Array.isArray(json.data) ? json.data : [json.data]);
   },
 };
