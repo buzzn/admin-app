89015fd01234c2636592ba0f00cfecca1c8dc427
Don Geolog <scsi.rdx@gmail.com>
2017-01-25 18:03:38 +0700

    dep and bootstrap updated (#51)

--- app/components/main_nav_bar/index.js
+++ app/components/main_nav_bar/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, Button } from 'reactstrap';
 import Auth from '@buzzn/module_auth';
 
-export const MainNavBar = ({ signedIn, dispatch, userMe }) => (
-  <nav className="navbar navbar-dark navbar-fixed-top bg-inverse">
-    <button type="button" className="navbar-toggler hidden-sm-up" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" aria-label="Toggle navigation"></button>
-    <a className="navbar-brand" href="#">Powergiver App</a>
-    <div id="navbar">
-      <nav className="nav navbar-nav float-xs-right">
-        { signedIn ?
-          <div>
-            <Link to={ `/users/${userMe}` } className="nav-item nav-link" activeOnlyWhenExact>My profile</Link>
-            &nbsp;
-            <button className="btn btn-outline-warning" onClick={ () => dispatch(Auth.actions.signOut()) }>Sign Out</button>
-          </div> :
-          <Link to="/" className="nav-item nav-link" activeOnlyWhenExact>Login</Link>
-        }
-      </nav>
-    </div>
-  </nav>
-);
+export class MainNavBar extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    const { signedIn, dispatch, userMe, isOpen } = this.props;
+
+    return (
+      <div>
+        <Navbar inverse fixed="top" toggleable light={ false } className="bg-inverse">
+          <NavbarToggler right onClick={ ::this.toggle } />
+          <NavbarBrand href="#">Powergiver App</NavbarBrand>
+          <Collapse isOpen={ isOpen } navbar>
+            <Nav className="ml-auto" navbar>
+              <div className="float-right">
+                { signedIn ?
+                  <div>
+                    <Link to={ `/users/${userMe}` } activeOnlyWhenExact>My profile</Link>
+                    &nbsp;
+                    <Button outline color="warning" onClick={ () => dispatch(Auth.actions.signOut()) }>Sign Out</Button>
+                  </div> :
+                  <Link to="/" activeOnlyWhenExact>Login</Link>
+                }
+              </div>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
