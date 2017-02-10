7ff1d0b981eb5a554887a794b6554daf13204c8b
ChipJuggler <scsi.rdx@gmail.com>
2017-02-10 12:14:46 +0700

    bubbles in group overview

--- app/components/group_overview/index.js
+++ app/components/group_overview/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Bubbles from '@buzzn/module_bubbles';
 import Groups from '../../groups';
 import Users from '../../users';
+import BubblesLayout from './bubbles_layout';
 
 import './style.scss';
 
@@ -31,7 +32,7 @@ export class GroupOverview extends Component {
       'Loading...'));
 
     return (
-      <div className="row">
+      <div className="row group-overview">
         <div className="col-12"><div className="title">{ group.attributes.name }</div></div>
         <div className="col-6">
           <div className="row">
@@ -46,7 +47,7 @@ export class GroupOverview extends Component {
             <div className="col-9">{ managersProfiles.join(', ') }</div>
           </div>
         </div>
-        <div className="col-6"><Bubbles.container /></div>
+        <div className="col-6"><Bubbles.container layout={ BubblesLayout } /></div>
       </div>
     );
   }
