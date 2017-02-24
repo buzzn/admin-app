e1705a46c4a143c7bf772decaba9db4a2a119c58
ChipJuggler <scsi.rdx@gmail.com>
2017-02-24 13:15:32 +0700

    deps updated & fix for undocumented route props usage

--- app/components/powertaker_resources/index.js
+++ app/components/powertaker_resources/index.js
@@ -4,10 +4,10 @@ import CoreDataContainer from './core_data';
 
 export default ({ match: { url, params: { groupId, userId } } }) => (
   <div>
-    <Route path={ `${url}/core-data` } groupId={ groupId } userId={ userId } component={ CoreDataContainer } />
-    <Route path={ `${url}/contracts` } groupId={ groupId } userId={ userId } render={ () => (<div>Contracts</div>) } />
-    <Route path={ `${url}/bank` } groupId={ groupId } userId={ userId } render={ () => (<div>Bank</div>) } />
-    <Route path={ `${url}/register` } groupId={ groupId } userId={ userId } render={ () => (<div>Register</div>) } />
-    <Route path={ `${url}/charts` } groupId={ groupId } userId={ userId } render={ () => (<div>Charts</div>) } />
+    <Route path={`${url}/core-data`} render={ () => <CoreDataContainer groupId={groupId} userId={userId} /> } />
+    <Route path={ `${url}/contracts` } render={ () => (<div>Contracts</div>) } />
+    <Route path={ `${url}/bank` } render={ () => (<div>Bank</div>) } />
+    <Route path={ `${url}/register` } render={ () => (<div>Register</div>) } />
+    <Route path={ `${url}/charts` } render={ () => (<div>Charts</div>) } />
   </div>
 );
