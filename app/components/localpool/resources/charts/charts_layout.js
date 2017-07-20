5ce8eb46264a90ba05b4c32ec59126c4ee5d4e9c
ChipJuggler <scsi.rdx@gmail.com>
2017-07-21 01:58:59 +0300

    charts with calendar

--- app/components/localpool/resources/charts/charts_layout.js
+++ app/components/localpool/resources/charts/charts_layout.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { DateTimePicker } from 'react-widgets';
 
-export default ({ Chart, constants, loading, limit, changeResolution, changePage }) => (
+export default ({ Chart, constants, loading, limit, changeDate, timestamp, changeResolution, changePage }) => (
   <div className="col-12 chart-wrapper">
     <div style={{ position: 'relative' }}>
       <div className="text-center">
+        <button className="btn btn-default btn-icon icon-lg btn-chart-prev fa fa-chevron-left fa-2x" onClick={ () => changePage('prev') } disabled={ loading }></button>
         <button className="btn btn-default year" onClick={ () => changeResolution(constants.RESOLUTIONS.YEAR_MONTH) } disabled={ loading }>Jahr</button>
         <button className="btn btn-default month" onClick={ () => changeResolution(constants.RESOLUTIONS.MONTH_DAY) } disabled={ loading }>Monat</button>
         <button className="btn btn-default day" onClick={ () => changeResolution(constants.RESOLUTIONS.DAY_MINUTE) } disabled={ loading }>Tag</button>
         <button className="btn btn-default hour" onClick={ () => changeResolution(constants.RESOLUTIONS.HOUR_MINUTE) } disabled={ loading }>Stunde</button>
+        <button className="btn btn-default btn-icon icon-lg btn-chart-next fa fa-chevron-right fa-2x" onClick={ () => changePage('next') } disabled={ loading || limit }></button>
+        <div className="row justify-content-center">
+          <div className="col-3">
+            <DateTimePicker value={ new Date(timestamp) } onChange={ newDate => changeDate(newDate) }/>
+          </div>
+        </div>
       </div>
-      <button className="btn btn-default btn-icon btn-circle icon-lg btn-chart-prev fa fa-chevron-left fa-2x" onClick={ () => changePage('prev') } disabled={ loading }></button>
-      <button className="btn btn-default btn-icon btn-circle icon-lg btn-chart-next pull-right fa fa-chevron-right fa-2x" onClick={ () => changePage('next') } disabled={ loading || limit }></button>
       <Chart layout="horizontal" />
       <div className="basic-loading" style={{
         width: '100%',
