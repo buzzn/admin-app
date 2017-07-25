66a36d9788448857d37f572a4ee9e1a17bebea86
ChipJuggler <scsi.rdx@gmail.com>
2017-07-25 10:30:46 +0300

    chart navigation styling

--- app/components/localpool/resources/charts/charts_layout.js
+++ app/components/localpool/resources/charts/charts_layout.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { DateTimePicker } from 'react-widgets';
+import { DateTimePicker, DropdownList } from 'react-widgets';
 
-export default ({ Chart, constants, loading, limit, changeDate, timestamp, changeResolution, changePage }) => (
+export default ({ Chart, constants, loading, limit, changeDate, timestamp, resolution, changeResolution, changePage }) => (
   <div className="col-12 chart-wrapper">
     <div style={{ position: 'relative' }}>
-      <div className="text-center">
-        <button className="btn btn-default btn-icon icon-lg btn-chart-prev fa fa-chevron-left fa-2x" onClick={ () => changePage('prev') } disabled={ loading }></button>
-        <button className="btn btn-default year" onClick={ () => changeResolution(constants.RESOLUTIONS.YEAR_MONTH) } disabled={ loading }>Jahr</button>
-        <button className="btn btn-default month" onClick={ () => changeResolution(constants.RESOLUTIONS.MONTH_DAY) } disabled={ loading }>Monat</button>
-        <button className="btn btn-default day" onClick={ () => changeResolution(constants.RESOLUTIONS.DAY_MINUTE) } disabled={ loading }>Tag</button>
-        <button className="btn btn-default hour" onClick={ () => changeResolution(constants.RESOLUTIONS.HOUR_MINUTE) } disabled={ loading }>Stunde</button>
-        <button className="btn btn-default btn-icon icon-lg btn-chart-next fa fa-chevron-right fa-2x" onClick={ () => changePage('next') } disabled={ loading || limit }></button>
+      <div className="chart-navigation">
         <div className="row justify-content-center">
-          <div className="col-3">
-            <DateTimePicker value={ new Date(timestamp) } onChange={ newDate => changeDate(newDate) }/>
+          <div className="col-3 resolution">
+            <DropdownList
+              onChange={ ({ value }) => changeResolution(value) }
+              disabled={ loading }
+              value={ resolution }
+              valueField="value"
+              textField="label"
+              data={[
+                { value: constants.RESOLUTIONS.YEAR_MONTH, label: 'Jahr' },
+                { value: constants.RESOLUTIONS.MONTH_DAY, label: 'Monat' },
+                { value: constants.RESOLUTIONS.DAY_MINUTE, label: 'Tag' },
+                { value: constants.RESOLUTIONS.HOUR_MINUTE, label: 'Stunde' },
+              ]}
+              className="resolution-select"
+            />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-3 timestamp">
+            <button className="btn btn-default btn-icon icon-lg btn-chart-prev fa fa-chevron-left fa-2x" onClick={ () => changePage('prev') } disabled={ loading }></button>
+            <DateTimePicker
+              value={ new Date(timestamp) }
+              onChange={ newDate => changeDate(newDate) }
+              className="timestamp-picker"
+            />
+            <button className="btn btn-default btn-icon icon-lg btn-chart-next fa fa-chevron-right fa-2x" onClick={ () => changePage('next') } disabled={ loading || limit }></button>
           </div>
         </div>
       </div>
-      <Chart layout="horizontal" />
+      <Chart layout="horizontal" withTitle={ false } />
       <div className="basic-loading" style={{
         width: '100%',
         height: '100%',
