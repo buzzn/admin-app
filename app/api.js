d33d4ec1bfb87fd76fbb0df470fc9862266054f5
ChipJuggler <scsi.rdx@gmail.com>
2017-09-21 21:48:04 +0300

    /me endpoint fix

--- app/api.js
+++ app/api.js
@@ -2,15 +2,15 @@ import 'whatwg-fetch';
 import { prepareHeaders, parseResponse, camelizeResponseKeys, snakeReq } from './_util';
 
 export default {
-  fetchUserMe({ token, apiUrl, apiPath }) {
-    return fetch(`${apiUrl}${apiPath}/me?include=address,bank_accounts`, {
+  fetchUserMe({ token, apiUrl }) {
+    return fetch(`${apiUrl}api/me?include=address,bank_accounts`, {
       headers: prepareHeaders(token),
     })
     .then(parseResponse)
     .then(camelizeResponseKeys);
   },
-  updateUserMe({ token, apiUrl, apiPath, params }) {
-    return fetch(`${apiUrl}${apiPath}/me`, {
+  updateUserMe({ token, apiUrl, params }) {
+    return fetch(`${apiUrl}api/me`, {
       headers: prepareHeaders(token),
       method: 'PATCH',
       body: JSON.stringify(snakeReq(params)),
