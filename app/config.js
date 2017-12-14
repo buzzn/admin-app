d4f196512478155a7b47265aa8dbdb4845d5fd5e
ChipJuggler <scsi.rdx@gmail.com>
2017-12-14 18:04:18 +0300

    develop branch is using production backend now. WARNING

--- app/config.js
+++ app/config.js
@@ -18,6 +18,15 @@ if (process.env.NODE_ENV === 'production') {
     callBackURL: '',
     secure: true,
   };
+} else if (process.env.NODE_ENV === 'develop') {
+  config = {
+    apiUrl: 'https://core.buzzn.io/',
+    apiPath: 'api/admin',
+    authPath: 'api/me',
+    clientId: '',
+    callBackURL: '',
+    secure: true,
+  };
 } else {
   config = {
     apiUrl: 'http://localhost:3000/',
