e408ea03c050979f42952c6a6000227641fa05d4
ChipJuggler <scsi.rdx@gmail.com>
2017-12-22 01:58:23 +0300

    link style

--- app/components/group_settings/index.js
+++ app/components/group_settings/index.js
@@ -213,8 +213,8 @@ class GroupSettings extends React.Component<Props> {
                     <Row className="fieldgroup">
                       <Col xs="4" className="fieldname"/>
                       <Col xs="8" className="grey-underline fieldvalue">
-                        <a href={ group.displayAppUrl } target="_blank" style={{ textDecoration: 'underline' }}>
-                          { group.slug }&nbsp;&nbsp;&nbsp;<i className="fa fa-external-link"/>
+                        <a href={ group.displayAppUrl } target="_blank">
+                          <span style={{ textDecoration: 'underline' }}>{ group.slug }</span>&nbsp;&nbsp;&nbsp;<i className="fa fa-external-link"/>
                         </a>
                       </Col>
                     </Row>
