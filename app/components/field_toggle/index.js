925f558eadbec48ff887f0e8fb31a70d01b0728e
ChipJuggler <scsi.rdx@gmail.com>
2017-12-22 02:08:15 +0300

    hack comment

--- app/components/field_toggle/index.js
+++ app/components/field_toggle/index.js
@@ -14,6 +14,7 @@ const FieldToggle = ({ input, submitForm, className, meta: { touched, error } }:
     <span className={ `field-toggle ${(touched && error) && 'has-danger'} ${className}` }>
       <span
         className={ `field-toggle-switch ${input.value ? 'on' : 'off'}` }
+        // FIXME: it's an "official" feature/bug. Please check git issues for setTimeout. Fix this "200" when possible.
         onClick={ () => { input.onChange(!input.value); setTimeout(submitForm, 200); } } />
       { touched && error && <span className="form-control-feedback">{ error }</span> }
     </span>
