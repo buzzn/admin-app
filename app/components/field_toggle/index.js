3931817ac2cc6fed945b8ab1f78cae05a96c8d9c
ChipJuggler <scsi.rdx@gmail.com>
2018-01-15 20:20:16 +0300

    Prevent extra submit from toggle switch.

--- app/components/field_toggle/index.js
+++ app/components/field_toggle/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 
 import './style.scss';
 
-const FieldToggle = ({ input, submitForm, className, meta: { touched, error } }) => (
-    <span className={`field-toggle ${touched && error && 'has-danger'} ${className}`}>
-      <span
-        className={`field-toggle-switch ${input.value ? 'on' : 'off'}`}
-        // FIXME: it's an "official" feature/bug. Please check git issues for setTimeout. Fix this "200" when possible.
-        onClick={() => {
-          input.onChange(!input.value);
-          setTimeout(submitForm, 200);
-        }}
-      />
-      {touched && error && <span className="form-control-feedback">{error}</span>}
-    </span>
+const FieldToggle = ({ input, submitForm, className, meta: { touched, error, submitting } }) => (
+  <span className={`field-toggle ${touched && error && 'has-danger'} ${className} ${submitting && 'submitting'}`}>
+    <span
+      className={`field-toggle-switch ${input.value ? 'on' : 'off'}`}
+      // FIXME: it's an "official" feature/bug. Please check git issues for setTimeout. Fix this "200" when possible.
+      onClick={() => {
+        if (submitting) return;
+        input.onChange(!input.value);
+        setTimeout(submitForm, 200);
+      }}
+    />
+    {touched && error && <span className="form-control-feedback">{error}</span>}
+  </span>
 );
 
 export default FieldToggle;
