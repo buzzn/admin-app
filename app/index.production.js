e58ab7c0ee63805cf0b3da9a0dfc339d3bb239cf
ChipJuggler <scsi.rdx@gmail.com>
2018-01-29 19:25:30 +0300

    moment locale

--- app/index.production.js
+++ app/index.production.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { IntlProvider, addLocaleData } from 'react-intl';
+import moment from 'moment';
 import en from 'react-intl/locale-data/en';
 import de from 'react-intl/locale-data/de';
 import allMessages from '@buzzn/i18n';
@@ -11,7 +12,8 @@ import Root from './root';
 addLocaleData([...en, ...de]);
 
 function language() {
-  let lang = navigator.language ||
+  let lang =
+    navigator.language ||
     (navigator.userLanguage && navigator.userLanguage.replace(/-[a-z]{2}$/, String.prototype.toUpperCase)) ||
     'de-DE';
   if (lang === 'en-GB') lang = 'en-US';
@@ -20,6 +22,8 @@ function language() {
     if (lang === 'de') lang = 'de-DE';
   }
   if (!['en-US', 'de-DE'].includes(lang)) lang = 'de-DE';
+  moment.locale(lang);
+  moment.updateLocale(moment.locale(), { invalidDate: '' });
   return lang;
 }
 
