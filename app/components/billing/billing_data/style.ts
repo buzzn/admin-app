856dc0b3a93369ecd17d90c695cf788852cf364c
ChipJuggler <scsi.rdx@gmail.com>
2018-03-08 12:53:39 +0300

    bricks grid fix

--- app/components/billing/billing_data/style.ts
+++ app/components/billing/billing_data/style.ts
@@ -47,23 +47,22 @@ export const MaLoListHeader = styled.div`
       font-size: 0.625rem;
       border-right: 1px solid #e0e0e0;
       border-left: 1px solid #e0e0e0;
-      background: -moz-linear-gradient(
-        to right,
-        ${({ ticks }: GridStyleProps) =>
-      ticks.map((t, i) =>
-        `${i !== 0 ? `white calc(${t}%),` : ''}#eeeeee calc(${t}% + 2px), white calc(${t}% + 4px)${
-          i < ticks.length - 1 ? ',' : ''
-        }`)}
-      );
-
-      background: linear-gradient(
-        to right,
-        ${({ ticks }: GridStyleProps) =>
-      ticks.map((t, i) =>
-        `${i !== 0 ? `white calc(${t}%),` : ''}#eeeeee calc(${t}% + 2px), white calc(${t}% + 3px)${
-          i < ticks.length - 1 ? ',' : ''
-        }`)}
-      );
+    background: -moz-linear-gradient(
+      to right,
+      ${({ ticks }: GridStyleProps) =>
+    ticks.map((t, i) =>
+      `${i !== 0 ? `white ${t}%,` : ''}#eeeeee ${t}%, #eeeeee calc(${t}% + 2px), white calc(${t}% + 2px)${
+        i < ticks.length - 1 ? ',' : ''
+      }`)}
+    );
+    background: linear-gradient(
+      to right,
+      ${({ ticks }: GridStyleProps) =>
+    ticks.map((t, i) =>
+      `${i !== 0 ? `white ${t}%,` : ''}#eeeeee ${t}%, #eeeeee calc(${t}% + 2px), white calc(${t}% + 2px)${
+        i < ticks.length - 1 ? ',' : ''
+      }`)}
+    );
       .month {
         width: 25%;
         padding-left: 4px;
@@ -97,7 +96,7 @@ export const MaLoRow = styled.div`
       to right,
       ${({ ticks }: GridStyleProps) =>
     ticks.map((t, i) =>
-      `${i !== 0 ? `white calc(${t}%),` : ''}#eeeeee calc(${t}% + 2px), white calc(${t}% + 4px)${
+      `${i !== 0 ? `white ${t}%,` : ''}#eeeeee ${t}%, #eeeeee calc(${t}% + 2px), white calc(${t}% + 2px)${
         i < ticks.length - 1 ? ',' : ''
       }`)}
     );
@@ -105,7 +104,7 @@ export const MaLoRow = styled.div`
       to right,
       ${({ ticks }: GridStyleProps) =>
     ticks.map((t, i) =>
-      `${i !== 0 ? `white calc(${t}%),` : ''}#eeeeee calc(${t}% + 2px), white calc(${t}% + 3px)${
+      `${i !== 0 ? `white ${t}%,` : ''}#eeeeee ${t}%, #eeeeee calc(${t}% + 2px), white calc(${t}% + 2px)${
         i < ticks.length - 1 ? ',' : ''
       }`)}
     );
