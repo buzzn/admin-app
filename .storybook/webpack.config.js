2eb676601ca5a7349968708709e26afe3cac15a2
ChipJuggler <scsi.rdx@gmail.com>
2018-03-27 14:23:27 +0300

    storybook fixes

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,13 +4,45 @@ const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/w
 module.exports = (baseConfig, env) => {
   const config = genDefaultConfig(baseConfig, env);
 
-  config.module.rules.push({
-    test: /\.scss$/,
-    loaders: ["style-loader", "css-loader", "sass-loader"],
-    include: path.resolve(__dirname, '../')
-  });
+  config.module.rules = config.module.rules.concat([
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    },
+    {
+      test: /\.(ts|tsx|js)$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              targets: {
+                browsers: ['last 2 versions', 'safari >= 7'],
+                modules: false,
+                debug: true,
+              },
+            },
+          ],
+          '@babel/stage-3',
+          '@babel/react',
+          '@babel/typescript',
+        ],
+        plugins: [
+          'react-hot-loader/babel',
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-syntax-class-properties',
+          '@babel/plugin-syntax-object-rest-spread',
+        ],
+      },
+    },
+  ]);
 
   config.resolve.modules.push(path.resolve(__dirname, '../app'));
+  config.resolve.extensions = config.resolve.extensions.concat(['.ts', '.tsx', '.js', '.json']);
   config.resolve.alias.moment$ = 'moment/moment.js';
 
   config.entry.manager = ['babel-polyfill', 'bootstrap-loader'].concat(config.entry.manager);
