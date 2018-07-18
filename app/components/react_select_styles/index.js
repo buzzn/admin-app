f0ca661a0bd37830d4430b5426cd833178d14cc2
ChipJuggler <scsi.rdx@gmail.com>
2018-07-18 14:32:26 +0300

    react-select styles fixes

--- app/components/react_select_styles/index.js
+++ app/components/react_select_styles/index.js
@@ -3,7 +3,7 @@ export const mainStyle = {
     ...base,
     marginRight: '-16px',
   }),
-  control: (base, state) => ({
+  control: (base, { isDisabled, isFocused }) => ({
     ...base,
     borderRadius: 0,
     width: '100%',
@@ -11,6 +11,8 @@ export const mainStyle = {
     border: 'none',
     borderBottom: 'solid 1px #e0e0e0',
     height: '3.36rem',
+    borderColor: isDisabled ? '#e0e0e0' : isFocused ? '#9e9e9e' : '#f5f5f5',
+    boxShadow: isFocused ? `0 0 0 1px #e0e0e0` : null,
     '&:hover': { borderColor: 'hsl(0, 0%, 80%)' },
   }),
   indicatorSeparator: (base, state) => ({
@@ -26,8 +28,9 @@ export const mainStyle = {
   //   backgroundRepeat: 'no-repeat',
   //   padding: '.5rem 1rem',
   // }),
-  option: (base, state) => ({
+  option: (base, { isSelected, isFocused }) => ({
     ...base,
+    backgroundColor: isSelected ? '#9e9e9e' : isFocused ? '#e0e0e0' : 'transparent',
     ':active': { backgroundColor: '#f5f5f5' },
     '&:hover': { backgroundColor: '#e0e0e0' },
   }),
