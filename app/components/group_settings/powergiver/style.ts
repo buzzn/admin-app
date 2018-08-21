a1b496221563aec7dbbeb44787ceca9db57fa964
ChipJuggler <scsi.rdx@gmail.com>
2018-08-21 14:39:08 +0300

    group owner radios styling fix

--- app/components/group_settings/powergiver/style.ts
+++ app/components/group_settings/powergiver/style.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const OwnerOptions = styled.div`
-padding-top: 1rem;
+  padding-top: 1rem;
+  padding-left: 1.5rem;
+  .custom-control {
+    padding-left: 0;
+  }
   label {
     position: initial;
     line-height: 1.5rem;
