542b9fa033a5a37640c1734b202a4e84152b9d71
ChipJuggler <scsi.rdx@gmail.com>
2018-09-03 19:08:59 +0300

    proper orgMarkets list

--- app/organizations/api.js
+++ app/organizations/api.js
@@ -8,8 +8,12 @@ export default {
       .then(camelizeResponseKeys);
   },
   fetchAvailableOrganizationMarkets({ token, apiUrl, apiPath }) {
-    return fetch(`${apiUrl}${apiPath}/organization_markets`, { headers: prepareHeaders(token) })
+    return fetch(`${apiUrl}${apiPath}/organization_markets?include=market_functions`, { headers: prepareHeaders(token) })
       .then(parseResponse)
-      .then(camelizeResponseKeys);
+      .then(camelizeResponseKeys)
+      .then(res => ({
+        ...res,
+        array: res.array.filter(o => o.marketFunctions.array.find(f => f.function === 'electricity_supplier')),
+      }));
   },
 };
