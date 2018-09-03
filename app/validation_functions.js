0da9449ef14d5b0b25ca864feb82897414389519
ChipJuggler <scsi.rdx@gmail.com>
2018-09-03 19:24:52 +0300

    date validator fix

--- app/validation_functions.js
+++ app/validation_functions.js
@@ -2,14 +2,11 @@ import moment from 'moment';
 
 export const required = value => (value ? undefined : 'Required');
 
-export const isString = value =>
-  (value === undefined || value === null || typeof value === 'string' ? undefined : 'Must be a string');
+export const isString = value => (value === undefined || value === null || typeof value === 'string' ? undefined : 'Must be a string');
 
-export const maxLength = max => value =>
-  (value === undefined || value === null || value.length < max ? undefined : `Must be ${max} characters or less`);
+export const maxLength = max => value => (value === undefined || value === null || value.length < max ? undefined : `Must be ${max} characters or less`);
 
-export const minLength = min => value =>
-  (value === undefined || value === null || value.length > min ? undefined : `Must be ${min} characters or more`);
+export const minLength = min => value => (value === undefined || value === null || value.length > min ? undefined : `Must be ${min} characters or more`);
 
 export const isNumber = value => (value === undefined || !isNaN(Number(value)) ? undefined : 'Must be a number');
 
@@ -17,25 +14,19 @@ export const minValue = min => value => (value === undefined || value > min ? un
 
 export const maxValue = max => value => (value === undefined || value < max ? undefined : `Must be at least ${max}`);
 
-export const minValueInc = min => value =>
-  (value === undefined || value >= min ? undefined : `Must be at least ${min}`);
+export const minValueInc = min => value => (value === undefined || value >= min ? undefined : `Must be at least ${min}`);
 
-export const maxValueInc = max => value =>
-  (value === undefined || value <= max ? undefined : `Must be at least ${max}`);
+export const maxValueInc = max => value => (value === undefined || value <= max ? undefined : `Must be at least ${max}`);
 
-export const isEmail = value =>
-  (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined);
+export const isEmail = value => (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined);
 
-export const isAlphaNumeric = value =>
-  (value && /[^a-zA-Z0-9 ]/i.test(value) ? 'Only alphanumeric characters' : undefined);
+export const isAlphaNumeric = value => (value && /[^a-zA-Z0-9 ]/i.test(value) ? 'Only alphanumeric characters' : undefined);
 
-export const isPhoneNumber = value =>
-  (value && !/^(0|[1-9][0-9]{9})$/i.test(value) ? 'Invalid phone number, must be 10 digits' : undefined);
+export const isPhoneNumber = value => (value && !/^(0|[1-9][0-9]{9})$/i.test(value) ? 'Invalid phone number, must be 10 digits' : undefined);
 
-export const isIncluded = list => value =>
-  (value === undefined || value === null || list.includes(value) ? undefined : 'Must be the one of the values');
+export const isIncluded = list => value => (value === undefined || value === null || list.includes(value) ? undefined : 'Must be the one of the values');
 
-export const isDate = value => (value === undefined || moment(value).isValid() ? undefined : 'Must be a valid date');
+export const isDate = value => (value === undefined || value === '' || moment(value).isValid() ? undefined : 'Must be a valid date');
 
 export const fieldValidator = (field) => {
   const validators = [];
