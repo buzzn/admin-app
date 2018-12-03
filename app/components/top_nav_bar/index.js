c24c430aa1177c3856c0badec224b9ee942145f6
ChipJuggler <scsi.rdx@gmail.com>
2018-12-03 22:13:36 +0100

    home link cy hook

--- app/components/top_nav_bar/index.js
+++ app/components/top_nav_bar/index.js
@@ -81,7 +81,7 @@ export class TopNavBar extends React.Component {
     return (
       <Navbar fixed="top" expand dark className={`new-top-nav-bar ${shrinked && 'shrinked'}`} {...hoverEvents}>
         <Container style={{ maxWidth: '1440px' }}>
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand" to="/" data-cy="home link">
             <img src={devMode ? DevLogoImg : LogoImg} />
           </Link>
           <NavbarToggler onClick={this.toggle} />
