64eca8ecba3a26e066a6dc0669078e6b0a75085d
ChipJuggler <scsi.rdx@gmail.com>
2019-03-18 13:12:33 +0100

    deps upd

--- app/hot_root.js
+++ app/hot_root.js
@@ -1,4 +1,37 @@
-import Root from 'root';
-import { hot } from 'react-hot-loader';
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import { Provider } from 'react-redux';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import moment from 'moment';
+import en from 'react-intl/locale-data/en';
+import de from 'react-intl/locale-data/de';
+import allMessages from '@buzzn/i18n';
+import store from './configure_store';
+import Root from './root';
 
-export default hot(module)(Root);
+addLocaleData([...en, ...de]);
+
+function language() {
+  let lang = navigator.language
+    || (navigator.userLanguage && navigator.userLanguage.replace(/-[a-z]{2}$/, String.prototype.toUpperCase))
+    || 'de-DE';
+  if (lang === 'en-GB') lang = 'en-US';
+  if (lang.length === 2) {
+    if (lang === 'en') lang = 'en-US';
+    if (lang === 'de') lang = 'de-DE';
+  }
+  if (!['en-US', 'de-DE'].includes(lang)) lang = 'de-DE';
+  moment.locale(lang);
+  moment.updateLocale(moment.locale(), { invalidDate: '' });
+  return lang;
+}
+
+const App = () => (
+  <IntlProvider locale={language()} messages={allMessages[language()]} onError={() => false}>
+    <Provider store={store}>
+      <Root />
+    </Provider>
+  </IntlProvider>
+);
+
+export default hot(App);
--- app/index.development.js
+++ app/index.development.js
@@ -1,37 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import moment from 'moment';
-import en from 'react-intl/locale-data/en';
-import de from 'react-intl/locale-data/de';
-import allMessages from '@buzzn/i18n';
-import store from './configure_store';
 import HotRoot from './hot_root';
 
-addLocaleData([...en, ...de]);
-
-function language() {
-  let lang =
-    navigator.language ||
-    (navigator.userLanguage && navigator.userLanguage.replace(/-[a-z]{2}$/, String.prototype.toUpperCase)) ||
-    'de-DE';
-  if (lang === 'en-GB') lang = 'en-US';
-  if (lang.length === 2) {
-    if (lang === 'en') lang = 'en-US';
-    if (lang === 'de') lang = 'de-DE';
-  }
-  if (!['en-US', 'de-DE'].includes(lang)) lang = 'de-DE';
-  moment.locale(lang);
-  moment.updateLocale(moment.locale(), { invalidDate: '' });
-  return lang;
-}
-
-ReactDOM.render(
-  <IntlProvider locale={language()} messages={allMessages[language()]} onError={() => false}>
-    <Provider store={store}>
-      <HotRoot />
-    </Provider>
-  </IntlProvider>,
-  document.querySelector('#root'),
-);
+ReactDOM.render(<HotRoot />, document.querySelector('#root'));
