fe5705e74107c7de8c976d5752c3ae5dad7dd1a5
Waleed <waleed.gatosorriso@gmail.com>
2021-01-07 10:58:54 +0100

    SDESK-339 Added if statement to make sure, the platform doesn't crash if name or id are missing

--- app/components/powertakers/powertakers_list/index.tsx
+++ app/components/powertakers/powertakers_list/index.tsx
@@ -36,15 +36,18 @@ const PowertakersList = ({
 }: Props & BreadcrumbsProps & InjectedIntlProps) => {
   if (loading) return <Loading minHeight={40} />;
 
-  const filteredPowertakers = powertakers.filter(o => (pType === 'active' ? o.status !== 'ended' : o.status === 'ended'));
+  const filteredPowertakers = powertakers.filter(o =>
+    pType === 'active' ? o.status !== 'ended' : o.status === 'ended',
+  );
 
   const prefix = 'admin.contracts';
 
   const data = orderBy(
     filteredPowertakers,
-    o => (o.type === 'contract_localpool_third_party'
-      ? null
-      : o.customer.name || `${o.customer.lastName} ${o.customer.firstName}`),
+    o =>
+      o.type === 'contract_localpool_third_party'
+        ? null
+        : o.customer.name || `${o.customer.lastName} ${o.customer.firstName}`,
     'asc',
   ).map(p => ({
     ...p,
@@ -52,21 +55,34 @@ const PowertakersList = ({
       p.type === 'contract_localpool_third_party'
         ? { value: 'drittbeliefert', image: DefaultThirdParty, type: 'avatar' }
         : p.customer.type === 'person'
-          ? {
+        ? {
             value: `${p.customer.lastName} ${p.customer.firstName}`,
             image: p.customer.image || DefaultPerson,
             type: 'avatar',
             clickable: true,
           }
-          : { value: p.customer.name, image: p.customer.image || DefaultOrganisation, type: 'avatar', clickable: true },
-    linkPowertaker: p.type === 'contract_localpool_third_party' ? '' : `${url}/${p.id}/powertaker`,
+        : {
+            value: p.customer.name,
+            image: p.customer.image || DefaultOrganisation,
+            type: 'avatar',
+            clickable: true,
+          },
+    linkPowertaker:
+      p.type === 'contract_localpool_third_party'
+        ? ''
+        : `${url}/${p.id}/powertaker`,
     linkContract: `${url}/${p.id}`,
-    marketLocationName: p.registerMeta.name,
+    marketLocationName:
+      p && p.registerMeta && p.registerMeta.name
+        ? p.registerMeta.name
+        : 'NONAME',
     // HACK
     linkMarketLocation: `${url
       .split('/')
       .slice(0, -1)
-      .join('/')}/market-locations/${p.registerMeta.id}`,
+      .join('/')}/market-locations/${
+      p && p.registerMeta && p.registerMeta.id ? p.registerMeta.id : 'NO-ID'
+    }`,
     beginDate: moment(p.beginDate).toDate(),
     lastDate: p.lastDate ? moment(p.lastDate).toDate() : p.lastDate,
     status: {
@@ -74,15 +90,20 @@ const PowertakersList = ({
       Display: (
         <div>
           <ContractStatus {...{ size: 'small', status: p.status }} />
-          <span className="ml-2">{intl.formatMessage({ id: `${prefix}.${p.status}` })}</span>
+          <span className="ml-2">
+            {intl.formatMessage({ id: `${prefix}.${p.status}` })}
+          </span>
         </div>
       ),
     },
   }));
-
   const columns = [
     {
-      Header: () => <TableParts.components.headerCell title={intl.formatMessage({ id: `${prefix}.tableName` })} />,
+      Header: () => (
+        <TableParts.components.headerCell
+          title={intl.formatMessage({ id: `${prefix}.tableName` })}
+        />
+      ),
       accessor: 'name',
       className: 'cy-powertaker',
       filterMethod: TableParts.filters.filterByValue,
@@ -91,7 +112,9 @@ const PowertakersList = ({
     },
     {
       Header: () => (
-        <TableParts.components.headerCell title={intl.formatMessage({ id: `${prefix}.tableMarketLocation` })} />
+        <TableParts.components.headerCell
+          title={intl.formatMessage({ id: `${prefix}.tableMarketLocation` })}
+        />
       ),
       accessor: 'marketLocationName',
       className: 'cy-malo',
@@ -102,7 +125,9 @@ const PowertakersList = ({
     },
     {
       Header: () => (
-        <TableParts.components.headerCell title={intl.formatMessage({ id: `${prefix}.tableContractNumber` })} />
+        <TableParts.components.headerCell
+          title={intl.formatMessage({ id: `${prefix}.tableContractNumber` })}
+        />
       ),
       accessor: 'fullContractNumber',
       className: 'cy-number',
@@ -113,12 +138,20 @@ const PowertakersList = ({
       sortMethod: TableParts.sort.sortByFulContractNumber,
     },
     {
-      Header: () => <TableParts.components.headerCell title={intl.formatMessage({ id: `${prefix}.tableBeginDate` })} />,
+      Header: () => (
+        <TableParts.components.headerCell
+          title={intl.formatMessage({ id: `${prefix}.tableBeginDate` })}
+        />
+      ),
       accessor: 'beginDate',
       Cell: ({ value }) => moment(value).format('DD.MM.YYYY'),
     },
     {
-      Header: () => <TableParts.components.headerCell title={intl.formatMessage({ id: `${prefix}.tableStatus` })} />,
+      Header: () => (
+        <TableParts.components.headerCell
+          title={intl.formatMessage({ id: `${prefix}.tableStatus` })}
+        />
+      ),
       accessor: 'status',
       filterMethod: TableParts.filters.filterByValue,
       sortMethod: TableParts.sort.sortByValue,
@@ -128,7 +161,11 @@ const PowertakersList = ({
 
   if (pType === 'past') {
     columns.splice(4, 0, {
-      Header: () => <TableParts.components.headerCell title={intl.formatMessage({ id: `${prefix}.tableLastDate` })} />,
+      Header: () => (
+        <TableParts.components.headerCell
+          title={intl.formatMessage({ id: `${prefix}.tableLastDate` })}
+        />
+      ),
       accessor: 'lastDate',
       Cell: ({ value }) => (value ? moment(value).format('DD.MM.YYYY') : ''),
     });
@@ -138,23 +175,28 @@ const PowertakersList = ({
     <React.Fragment>
       <PageTitle
         {...{
-          breadcrumbs: breadcrumbs.concat([{ id: '-----', title: 'Powertakers' }]),
+          breadcrumbs: breadcrumbs.concat([
+            { id: '-----', title: 'Powertakers' },
+          ]),
           title: intl.formatMessage({ id: 'admin.contracts.backPowertakers' }),
         }}
       />
       <CenterContent>
         <SubNavAddLink>
-          {!!group.allowedActions
-            && group.allowedActions.createLocalpoolThirdPartyContract === true && (
+          {!!group.allowedActions &&
+            group.allowedActions.createLocalpoolThirdPartyContract === true && (
               <Link to={`${url}/add-third-party`} data-cy="add third party CTA">
-                <FormattedMessage id="admin.contracts.addNewThirdParty" /> <i className="fa fa-tty" />
+                <FormattedMessage id="admin.contracts.addNewThirdParty" />{' '}
+                <i className="fa fa-tty" />
               </Link>
-          )}
-          {!!group.allowedActions && group.allowedActions.createLocalpoolPowerTakerContract === true && (
-            <Link to={`${url}/add-powertaker`} data-cy="add powertaker CTA">
-              <FormattedMessage id="admin.contracts.addNew" /> <i className="fa fa-user" />
-            </Link>
-          )}
+            )}
+          {!!group.allowedActions &&
+            group.allowedActions.createLocalpoolPowerTakerContract === true && (
+              <Link to={`${url}/add-powertaker`} data-cy="add powertaker CTA">
+                <FormattedMessage id="admin.contracts.addNew" />{' '}
+                <i className="fa fa-user" />
+              </Link>
+            )}
         </SubNavAddLink>
         <SubNav>
           <NavLink to={`${url}/active`} exact className="nav-link">
@@ -171,11 +213,16 @@ const PowertakersList = ({
               columns,
               getTdProps: (_state, rowInfo, column) => ({
                 onClick: (_e, handleOriginal) => {
-                  if (column.id === 'marketLocationName') history.push(rowInfo.original.linkMarketLocation);
-                  if (column.id === 'name' && rowInfo.original.linkPowertaker.length) {
+                  if (column.id === 'marketLocationName')
+                    history.push(rowInfo.original.linkMarketLocation);
+                  if (
+                    column.id === 'name' &&
+                    rowInfo.original.linkPowertaker.length
+                  ) {
                     history.push(rowInfo.original.linkPowertaker);
                   }
-                  if (column.id === 'fullContractNumber') history.push(rowInfo.original.linkContract);
+                  if (column.id === 'fullContractNumber')
+                    history.push(rowInfo.original.linkContract);
                   if (handleOriginal) handleOriginal();
                 },
               }),
