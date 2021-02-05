2c1f1170ef1e0048564d4cf5460c0e0a00349c7a
Danny Stey <mail@danny-stey.de>
2021-02-05 12:46:15 +0100

    log error on powertaker update contract

--- app/components/contract/powertaker/index.tsx
+++ app/components/contract/powertaker/index.tsx
@@ -499,9 +499,5 @@ const PowertakerContract = ({
 };
 
 export default injectIntl(
-  withEditOverlay(
-    reduxForm({ form: 'LPTUpdateForm', enableReinitialize: true })(
-      PowertakerContract,
-    ),
-  ),
+  withEditOverlay(reduxForm({ form: 'LPTUpdateForm', enableReinitialize: true })(PowertakerContract))
 );
